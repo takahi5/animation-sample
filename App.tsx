8bfd04d4c7e4cfb6acbf27058d1881a209333cd3
Takahiko Wada <wada.takahiko@gmail.com>
2023-02-24 19:35:59 +0400

    uploader sample

--- App.tsx
+++ App.tsx
@@ -1,59 +1,93 @@
 import React, { useRef, useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Pressable, StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet, View, Text, Button } from "react-native";
 import Lottie from "lottie-react-native";
 
 const callFakeAPI = async () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("success");
-    }, 2000);
+    }, 5000);
   });
+};
+
+type StatusItemProps = {
+  text: string;
+  status: "processing" | "completed" | "none";
+};
+const StatusItem = ({ text, status }: StatusItemProps) => {
+  const animationProcessing: React.LegacyRef<Lottie> | undefined = useRef(null);
+  const animationCompleted: React.LegacyRef<Lottie> | undefined = useRef(null);
+
+  useEffect(() => {
+    if (status === "processing") {
+      // temp ios fix begin
+      // @see https://github.com/lottie-react-native/lottie-react-native/issues/832
+      setTimeout(() => {
+        animationProcessing.current?.play();
+      }, 0);
+    } else if (status === "completed") {
+      // temp ios fix begin
+      // @see https://github.com/lottie-react-native/lottie-react-native/issues/832
+      setTimeout(() => {
+        animationCompleted.current?.play();
+      }, 0);
+    }
+  }, [status]);
+
+  return (
+    <View style={styles.itemContainer}>
+      <Text style={{ color: status === "processing" ? "black" : "grey" }}>
+        {text}
+      </Text>
+      <View style={styles.icon}>
+        {status === "processing" ? (
+          <Lottie
+            source={require("./assets/processing.json")}
+            ref={animationProcessing}
+            loop={true}
+          />
+        ) : status === "completed" ? (
+          <Lottie
+            source={require("./assets/checkmark.json")}
+            ref={animationCompleted}
+            loop={false}
+          />
+        ) : null}
+      </View>
+    </View>
+  );
 };
 
 export default function App() {
   const animation: React.LegacyRef<Lottie> | undefined = useRef(null);
-  const [isLiked, setIsLiked] = useState(false);
+  const [step, setStep] = useState(0);
 
-  useEffect(() => {
-    init();
-  }, []);
-
-  const init = () => {
-    // temp ios fix begin
-    // @see https://github.com/lottie-react-native/lottie-react-native/issues/832
-    setTimeout(() => {
-      animation.current?.play(0, 1);
-    }, 0);
-  };
-
-  const like = () => {
-    // temp ios fix begin
-    // @see https://github.com/lottie-react-native/lottie-react-native/issues/832
-    setTimeout(() => {
-      animation.current?.play(0, 19);
-    }, 0);
-  };
-
-  const onPressLike = () => {
-    if (isLiked) {
-      init();
-      setIsLiked(false);
-    } else {
-      like();
-      setIsLiked(true);
-    }
+  const upload = async () => {
+    setStep(1);
+    await callFakeAPI();
+    setStep(2);
+    await callFakeAPI();
+    setStep(3);
+    await callFakeAPI();
+    setStep(4);
   };
 
   return (
     <View style={styles.container}>
-      <Pressable onPress={onPressLike} style={{ width: 200, height: 200 }}>
-        <Lottie
-          source={require("./assets/like.json")}
-          ref={animation}
-          loop={false}
-        />
-      </Pressable>
+      <StatusItem
+        text="動画をアップロードしています"
+        status={step > 1 ? "completed" : step === 1 ? "processing" : "none"}
+      />
+      <StatusItem
+        text="サムネイル画像を作成しています"
+        status={step > 2 ? "completed" : step === 2 ? "processing" : "none"}
+      />
+      <StatusItem
+        text="動画を最適化しています"
+        status={step > 3 ? "completed" : step === 3 ? "processing" : "none"}
+      />
+      <Button onPress={upload} title="アップロード" />
       <StatusBar style="auto" />
     </View>
   );
@@ -65,5 +99,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  itemContainer: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: 300,
+  },
+  icon: {
+    width: 60,
+    height: 60,
   },
 });
