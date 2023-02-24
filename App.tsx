d4c73db62dfb33b3372383357546ca8d89f02fc5
Takahiko Wada <wada.takahiko@gmail.com>
2023-02-24 17:14:43 +0400

    play, pause, reset

--- App.tsx
+++ App.tsx
@@ -1,60 +1,45 @@
 import React, { useRef, useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Button, SafeAreaView } from "react-native";
 import Lottie from "lottie-react-native";
-
-const callFakeAPI = async () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("success");
-    }, 3000);
-  });
-};
 
 export default function App() {
   const animation: React.LegacyRef<Lottie> | undefined = useRef(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  useEffect(() => {
+  const play = () => {
     // temp ios fix begin
     // @see https://github.com/lottie-react-native/lottie-react-native/issues/832
     setTimeout(() => {
       animation.current?.play();
     }, 0);
+  };
 
-    // ダミーのAPIを呼ぶ
-    callFakeAPI().then(() => {
-      setIsLoading(false);
-    });
-  }, []);
+  const pause = () => {
+    setTimeout(() => {
+      animation.current?.pause();
+    }, 0);
+  };
+
+  const reset = () => {
+    setTimeout(() => {
+      animation.current?.reset();
+    }, 0);
+  };
 
   return (
-    <View style={styles.container}>
-      {isLoading ? (
-        <Lottie
-          style={{
-            width: 100,
-          }}
-          source={require("./assets/loading.json")}
-          ref={animation}
-          loop
-          autoPlay
-        />
-      ) : (
-        <View style={{ padding: 10 }}>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-        </View>
-      )}
+    <SafeAreaView style={styles.container}>
+      <Lottie
+        source={require("./assets/happy-valentines-day.json")}
+        ref={animation}
+        loop
+      />
+      <View>
+        <Button onPress={play} title="Play" />
+        <Button onPress={pause} title="Pause" />
+        <Button onPress={reset} title="Reset" />
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -62,7 +47,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
