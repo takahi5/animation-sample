739c85993d1d9e610dd6c6ba41de04d6ff18190c
Takahiko Wada <wada.takahiko@gmail.com>
2023-02-24 17:02:56 +0400

    sample of loadig screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import Lottie from "lottie-react-native";
+
+const callFakeAPI = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("success");
+    }, 3000);
+  });
+};
 
 export default function App() {
   const animation: React.LegacyRef<Lottie> | undefined = useRef(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     // temp ios fix begin
@@ -12,11 +21,38 @@ export default function App() {
     setTimeout(() => {
       animation.current?.play();
     }, 0);
+
+    // ダミーのAPIを呼ぶ
+    callFakeAPI().then(() => {
+      setIsLoading(false);
+    });
   }, []);
 
   return (
     <View style={styles.container}>
-      <Lottie source={require("./assets/loading.json")} ref={animation} loop />
+      {isLoading ? (
+        <Lottie
+          style={{
+            width: 100,
+          }}
+          source={require("./assets/loading.json")}
+          ref={animation}
+          loop
+          autoPlay
+        />
+      ) : (
+        <View style={{ padding: 10 }}>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
